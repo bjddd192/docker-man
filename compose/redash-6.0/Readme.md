4a543bf77f053c888c4a980a7255b07255882a67
yanglei <bjddd192@163.com>
2019-02-14 22:39:04 +0800

    优化 redash

--- compose/redash-6.0/Readme.md
+++ compose/redash-6.0/Readme.md
@@ -3,6 +3,13 @@
 ## 操作说明
 
 ```sh
+yum install epel-release
+yum ​install pwgen
+
+# setup.sh 脚本用于生成 env 文件
+# generate_key.sh 脚本用于更新 env 文件
+# 这里我使用 setup.sh 生成了一份 env 文件并将内容复制给了 env_file 文件 
+
 # 初始化系统数据（pg 数据库）
 docker-compose run --rm server create_db
 
---
+++ compose/redash-6.0/setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# This script setups dockerized Redash on Ubuntu 18.04.
+set -eu
+
+REDASH_BASE_PATH=/data/docker_volumn/redash
+
+create_directories() {
+    if [[ ! -e $REDASH_BASE_PATH ]]; then
+        sudo mkdir -p $REDASH_BASE_PATH
+        sudo chown $USER:$USER $REDASH_BASE_PATH
+    fi
+
+    if [[ ! -e $REDASH_BASE_PATH/postgres-data ]]; then
+        mkdir $REDASH_BASE_PATH/postgres-data
+    fi
+}
+
+create_config() {
+    if [[ -e $REDASH_BASE_PATH/env ]]; then
+        rm $REDASH_BASE_PATH/env
+        touch $REDASH_BASE_PATH/env
+    fi
+
+    COOKIE_SECRET=$(pwgen -1s 32)
+    POSTGRES_PASSWORD=$(pwgen -1s 32)
+    REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
+
+    echo "PYTHONUNBUFFERED=0" >> $REDASH_BASE_PATH/env
+    echo "REDASH_LOG_LEVEL=INFO" >> $REDASH_BASE_PATH/env
+    echo "REDASH_REDIS_URL=redis://redis:6379/0" >> $REDASH_BASE_PATH/env
+    echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
+    echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
+    echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
+}
+
+create_directories
+create_config
